97405d98ae25c65af0872ab45648ed000151efd6
MichaelRayven <themikerayven@gmail.com>
2022-04-18 23:03:02 +0700

    Created BookViewModel.java using RxJava

---
+++ app/src/main/java/com/example/readative/interfaces/BookDataSource.java
@@ -0,0 +1,31 @@
+package com.example.readative.interfaces;
+
+import com.example.readative.room.entity.Author;
+import com.example.readative.room.entity.Book;
+import com.example.readative.room.entity.Bookmark;
+import com.example.readative.room.entity.Genre;
+import com.example.readative.room.entity.Language;
+import com.example.readative.room.entity.PartOfSeries;
+import com.example.readative.room.entity.Quote;
+import com.example.readative.room.entity.Review;
+import com.example.readative.room.entity.Series;
+import com.example.readative.room.entity.Shelf;
+import com.example.readative.room.relationships.FileAndArchive;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Flowable;
+
+public interface BookDataSource {
+    Flowable<Book> getBook();
+    Flowable<Author> getAuthor();
+    Flowable<Review> getReview();
+
+    Flowable<List<FileAndArchive>> getFiles();
+    Flowable<List<Language>> getLanguages();
+    Flowable<List<Quote>> getQuotes();
+    Flowable<List<Genre>> getGenres();
+    Flowable<List<Bookmark>> getBookmarks();
+    Flowable<List<PartOfSeries>> getSeries();
+    Flowable<List<Shelf>> getShelves();
+}
--- app/src/main/java/com/example/readative/viewmodel/BookViewModel.java
+++ app/src/main/java/com/example/readative/viewmodel/BookViewModel.java
@@ -1,6 +1,48 @@
 package com.example.readative.viewmodel;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.readative.interfaces.BookDataSource;
+import com.example.readative.room.entity.Book;
+
+import org.reactivestreams.Subscriber;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Flowable;
+
 public class BookViewModel extends ViewModel {
+    private final BookDataSource mDataSource;
+
+    public BookViewModel(BookDataSource dataSource) {
+        mDataSource = dataSource;
+    }
+
+    public Flowable<Bitmap> getCover() {
+        return mDataSource.getBook().map(book -> BitmapFactory.decodeFile(book.coverUrl));
+    }
+
+    public LiveData<String> getTitle() {
+        return mDataSource.getBook().map(book -> book.title);
+    }
+
+    public Flowable<String> getDescription() {
+        return mDataSource.getBook().map(book -> book.description);
+    }
+
+    public Flowable<String> getAuthor() {
+        return mDataSource.getAuthor().map(author -> {
+            StringBuilder builder = new StringBuilder(author.firstName);
+            if (author.middleName != null) builder.append(", " + author.middleName);
+            if (author.lastName != null) builder.append(", " + author.lastName);
+            return builder.toString();
+        });
+    }
+
+
 }
